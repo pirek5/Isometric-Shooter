d016938534deadbcffce6d61e1e362bd63ab2e6b
Piotr Czajkowski <pir.czajkowski@gmail.com>
2023-02-07 13:07:55 +0100

    refactor:  player weapons controller

--- Assets/Scripts/Player/PlayerWeaponController.cs
+++ Assets/Scripts/Player/PlayerWeaponController.cs
@@ -1,83 +1,94 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using IsoShooter.Player;
 using IsoShooter.Weapons;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-public class PlayerWeaponController : MonoBehaviour
+namespace IsoShooter.Player
 {
-    [SerializeField]
-    private string _selectedWeapon;
-    [SerializeField]
-    private WeaponsDatabase _weaponsDatabase;
-    [SerializeField]
-    private Transform _weaponSlot;
-    
-    private PlayerSettings _playerSettings;
-    private ICharacterInput _playerInput;
-    private Weapon _currentWeapon;
-    private Transform _weaponTransform;
-
-    public void Initialize(ICharacterInput input, PlayerSettings playerSettings)
+    public class PlayerWeaponController : MonoBehaviour
     {
-        _playerSettings = playerSettings;
-        _playerInput = input;
-        CreateSelectedWeapon();
-        _playerInput.OnFirePerformed += HandleFirePerformed;
-        _playerInput.OnFireCanceled += HandleFireCanceled;
-    }
-
-    public void CleanUp()
-    {
-        _playerInput.OnFirePerformed -= HandleFirePerformed;
-        _playerInput.OnFireCanceled -= HandleFireCanceled;
-    }
-
-    private void Update()
-    {
-        RotateGun();
-    }
-
-    private void CreateSelectedWeapon()
-    {
-        Weapon weaponPrefab = _weaponsDatabase.GetWeaponPrefab(_selectedWeapon);
-        _currentWeapon = Instantiate(weaponPrefab, _weaponSlot);
-        _currentWeapon.InitializeWeapon();
-        _weaponTransform = _currentWeapon.GetComponent<Transform>();
-    }
-
-    private void HandleFirePerformed()
-    {
-        _currentWeapon?.HandleFirePerformed();
-    }
-
-    private void HandleFireCanceled()
-    {
-        _currentWeapon?.HandleFireCanceled();
-    }
-
-    private void HandleReloadPerformed()
-    {
-        _currentWeapon?.HandleReloadPerformed();
-    }
-    
-    private void RotateGun()
-    {
-        if(_weaponTransform == null)
-            return;
+        [SerializeField]
+        private string _selectedWeapon;
+        [SerializeField]
+        private Transform _weaponSlot;
         
-        Vector3 gunAimPosition = _playerInput.AimDestination;
-        if (gunAimPosition.y < _playerSettings.MinAimingHeight)
+        private PlayerSettings _playerSettings;
+        private WeaponsDatabase _weaponsDatabase;
+        private ICharacterInput _playerInput;
+        private Weapon _currentWeapon;
+
+
+        public void Initialize(ICharacterInput input, PlayerSettings playerSettings, WeaponsDatabase weaponsDatabase)
         {
-            gunAimPosition = new Vector3(gunAimPosition.x, _playerSettings.MinAimingHeight, gunAimPosition.z);
+            _playerSettings = playerSettings;
+            _weaponsDatabase = weaponsDatabase;
+            _playerInput = input;
+            CreateSelectedWeapon();
+            
+            _playerInput.OnFirePerformed += HandleFirePerformed;
+            _playerInput.OnFireCanceled += HandleFireCanceled;
+            _playerInput.OnReloadPerformed += HandleReloadPerformed;
         }
+
+        public void CleanUp()
+        {
+            _playerInput.OnFirePerformed -= HandleFirePerformed;
+            _playerInput.OnFireCanceled -= HandleFireCanceled;
+            _playerInput.OnReloadPerformed -= HandleReloadPerformed;
+        }
+
+        private void Update()
+        {
+            RotateGun();
+        }
+
+        private void CreateSelectedWeapon()
+        {
+            Weapon weaponPrefab = _weaponsDatabase.GetWeaponPrefab(_selectedWeapon);
+            _currentWeapon = Instantiate(weaponPrefab, _weaponSlot);
+            _currentWeapon.InitializeWeapon();
+        }
+
+        private void HandleFirePerformed()
+        {
+            if(_currentWeapon == null)
+                return;
             
-        float distanceToAim = Vector3.Distance(_weaponSlot.position, gunAimPosition);
-        if(distanceToAim < _playerSettings.MinGunAimingDistance)
-            return;
+            _currentWeapon.HandleFirePerformed();
+        }
+
+        private void HandleFireCanceled()
+        {
+            if(_currentWeapon == null)
+                return;
             
-        _weaponTransform.transform.LookAt(_playerInput.AimDestination);
+            _currentWeapon.HandleFireCanceled();
+        }
+
+        private void HandleReloadPerformed()
+        {
+            if(_currentWeapon == null)
+                return;
+            
+            _currentWeapon.HandleReloadPerformed();
+        }
+        
+        private void RotateGun()
+        {
+            if(_currentWeapon == null)
+                return;
+            
+            Vector3 gunAimPosition = _playerInput.AimDestination;
+            if (gunAimPosition.y < _playerSettings.MinAimingHeight)
+            {
+                gunAimPosition = new Vector3(gunAimPosition.x, _playerSettings.MinAimingHeight, gunAimPosition.z);
+            }
+                
+            float distanceToAim = Vector3.Distance(_weaponSlot.position, gunAimPosition);
+            if(distanceToAim < _playerSettings.MinGunAimingDistance)
+                return;
+                
+            _currentWeapon.transform.LookAt(_playerInput.AimDestination);
+        }
     }
 }
+
+
